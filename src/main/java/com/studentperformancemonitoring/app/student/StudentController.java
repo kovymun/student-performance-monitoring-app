a06badd81de39ff83efa283a98b1569750b75655
Kovy <codingstudent2021@gmail.com>
2024-11-28 19:19:15 +0200

    Amending Controller class in-line comments

--- src/main/java/com/studentperformancemonitoring/app/student/StudentController.java
+++ src/main/java/com/studentperformancemonitoring/app/student/StudentController.java
@@ -1,26 +1,29 @@
 package com.studentperformancemonitoring.app.student;
 
+// Importing dependencies
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
-@RequestMapping(path = "api")
+@RestController // annotation to mark class as a REST controller to handle HTTP requests/responses
+@RequestMapping(path = "api") // Base URL mapping for the controller functions
 public class StudentController {
 
     private final StudentService studentService;
 
-    @Autowired
+    // Constructor for injecting the service layer into the Student controller class    @Autowired
     public StudentController(StudentService studentService) {
         this.studentService = studentService;
     }
 
+    // Controller function that handles GET requests to retrieve a list of students and their performance results.
     @GetMapping("/students")
     public List<Student> getStudents() {
         return studentService.getStudents();
     }
 
+    // Controller function that handles POST requests to add a new student to the DB.
     @PostMapping("/new-student")
     public void createStudent(@RequestBody Student student) {
 
